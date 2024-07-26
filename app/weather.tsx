1df4fb975f5057f2d73e6f7dd44f3f23eef6eb42
si-mrami <si.mohamedrami1902@gmail.com>
2024-07-26 20:23:21 +0100

    init commit

---
+++ app/weather.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const weather = () => {
+  return (
+    <View>
+      <Text>weather</Text>
+    </View>
+  );
+};
+
+export default weather;
+
+const styles = StyleSheet.create({});
